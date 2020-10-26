cb89f8c4d195d5734aca91735d6e86852969bc4d
Austin Clyde <aclyde@uchicago.edu>
2020-10-26 08:50:01 -0500

    Initial commit

---
+++ load_graph.py
@@ -0,0 +1,75 @@
+import pickle
+import random
+
+import dgl
+import numpy as np
+import torch as th
+from dgl.data import citation_graph as citegrh
+
+def load_reddit():
+    from dgl.data import RedditDataset
+
+    # load reddit data
+    data = RedditDataset(self_loop=True)
+    g = data[0]
+    g.ndata['features'] = g.ndata['feat']
+    g.ndata['labels'] = g.ndata['label']
+    return g, data.num_labels
+
+def inductive_split(g):
+    """Split the graph into training graph, validation graph, and test graph by training
+    and validation masks.  Suitable for inductive models."""
+    train_g = g.subgraph(g.ndata['train_mask'])
+    val_g = g.subgraph(g.ndata['train_mask'] | g.ndata['val_mask'])
+    test_g = g
+    return train_g, val_g, test_g
+
+def load_cora_data():
+    data = citegrh.load_cora()
+    features = th.FloatTensor(data.features)
+    labels = th.LongTensor(data.labels)
+    train_mask = th.BoolTensor(data.train_mask)
+    test_mask = th.BoolTensor(data.test_mask)
+    g = dgl.DGLGraph(data.graph)
+    g.add_edges(g.nodes(), g.nodes())
+
+    return g, features, labels, train_mask, test_mask
+
+
+def load_mol_data(dsize):
+    with open("test_data.pkl", 'rb') as f:
+        data = pickle.load(f)
+
+    networkx_graph = data['g']
+    networkx_graph.add_edges_from(zip(networkx_graph.nodes(), networkx_graph.nodes())) # add self edges
+
+    graph = dgl.DGLGraph(networkx_graph)
+
+    features = th.FloatTensor(data['features'])
+    labels = th.FloatTensor(data['labels'])[:, [0]]
+
+    graph.ndata['features'] = features
+    graph.ndata['labels'] = labels
+
+    train_mask = [random.random() < dsize for _ in range(features.shape[0])]
+    train_mask = np.array(train_mask, dtype=np.bool).flatten()
+    # test_mask = [random.random() < 0.3 for _ in range(features.shape[0])]
+    test_mask = ~train_mask
+
+    graph.ndata['train_mask'] = th.BoolTensor(train_mask)
+    graph.ndata['test_mask'] = th.BoolTensor(test_mask)
+
+    # sampler = dgl.dataloading.MultiLayerFullNeighborSampler(2)
+    # dataloader = dgl.dataloading.NodeDataLoader(
+    #     graph, np.where(train_mask)[0].flatten(), sampler,
+    #     batch_size=64,
+    #     shuffle=True,
+    #     drop_last=False)
+    #
+    # test_dataloader = dgl.dataloading.NodeDataLoader(
+    #     graph, np.where(test_mask)[0].flatten(), sampler,
+    #     batch_size=64,
+    #     shuffle=True,
+    #     drop_last=False)
+
+    return graph
---
+++ main.py
@@ -0,0 +1,133 @@
+import pickle
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
+from sklearn import metrics
+import random
+import dgl
+import dgl.function as fn
+import torch as th
+import torch.nn as nn
+import torch.nn.functional as F
+from dgl import DGLGraph
+import dgl.nn
+import torch
+from dgl.data import citation_graph as citegrh
+import networkx as nx
+
+import time
+import numpy as np
+from tqdm import tqdm
+
+from load_graph import load_mol_data
+
+def trainable_count(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+class GCN(nn.Module):
+    def __init__(self, in_dim, hidden_dim, out_dim, dropout=0.2):
+        super(GCN, self).__init__()
+        self.d_layer1 = nn.Linear(in_dim, hidden_dim)
+        self.n1 = nn.BatchNorm1d(hidden_dim)
+        self.g_layer1 = dgl.nn.SAGEConv(hidden_dim, hidden_dim, aggregator_type='mean', activation=F.relu)
+        self.d_layer2 = nn.Linear(hidden_dim, hidden_dim)
+        self.n2 = nn.BatchNorm1d(hidden_dim)
+        self.g_layer2 = dgl.nn.SAGEConv(hidden_dim, hidden_dim, aggregator_type='mean', activation=F.relu)
+        self.d_layer3 = nn.Linear(hidden_dim, out_dim)
+        self.dropout = nn.Dropout(dropout)
+
+
+
+    def forward(self, g, h):
+        h = self.d_layer1(h)
+        h = self.n1(h)
+        h = F.relu(h)
+        h = self.dropout(h)
+
+        h = self.g_layer1(g,h)
+
+        h = self.d_layer2(h)
+        h = self.n2(h)
+        h = F.relu(h)
+        h = self.dropout(h)
+
+        h = self.g_layer2(g, h)
+
+        h = F.relu(self.d_layer3(h))
+
+        return h
+
+
+def evaluate(model, g, features, labels, mask, G):
+    model.eval()
+    with th.no_grad():
+        logp = net(g, features)
+        preds, labels = logp[mask], labels[mask]
+        print(preds.shape, labels.shape)
+        loss = F.mse_loss(preds, labels)
+
+        print('r2', metrics.r2_score(labels, preds))
+
+
+        # nx.draw(G, pos=nx.circular_layout(G), node_color=labels_)
+        # plt.show()
+        #
+        # labels = labels[mask]
+        # logp = logp[mask]
+
+
+
+        return loss
+
+
+
+
+g = load_mol_data(0.8)
+
+net = GCN(1302, 32, 1)
+
+print("trainbale:", trainable_count(net))
+optimizer = th.optim.Adam(net.parameters(), lr=5e-4)
+dur = []
+t0 = time.time()
+for epoch in range(10000):
+    net.train()
+
+    for i in tqdm(range(200)):
+
+        if epoch >= 3:
+            dur.append(time.time() - t0)
+
+
+        if epoch >=3:
+            t0 = time.time()
+        optimizer.zero_grad()
+
+        logits = net(g, g.ndata['features'])
+        logp = (logits)
+
+        rid = np.random.randint(0, len(np.where(g.ndata['train_mask'])[0]), 128)
+
+        loss = F.mse_loss(logp[g.ndata['train_mask']][rid], g.ndata['labels'][g.ndata['train_mask']][rid])
+
+        loss.backward()
+        optimizer.step()
+
+    optimizer.zero_grad()
+
+    logits = net(g, g.ndata['features'])
+    logp = (logits)
+
+
+    loss = F.mse_loss(logp[g.ndata['train_mask']], g.ndata['labels'][g.ndata['train_mask']])
+
+    loss.backward()
+    optimizer.step()
+
+    if epoch >=3:
+        dur.append(time.time() - t0)
+
+    acc = evaluate(net, g, g.ndata['features'], g.ndata['labels'], g.ndata['test_mask'], g)
+    print("Epoch {:05d} | Loss {:.4f} | Test Acc {:.4f} | Time(s) {:.4f}".format(
+            epoch, loss.item(), acc, np.mean(dur)))
